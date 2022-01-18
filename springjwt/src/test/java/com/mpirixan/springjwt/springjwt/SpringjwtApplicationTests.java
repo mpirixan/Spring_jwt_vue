b7f5ad72173cbebdce5fcedba5ba064b38c5231c
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2022-01-18 12:41:49 -0300

    backend-spring add

---
+++ springjwt/src/test/java/com/mpirixan/springjwt/springjwt/SpringjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpirixan.springjwt.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
